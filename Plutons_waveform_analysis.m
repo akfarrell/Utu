10f5ca585df2fa2b7f6a55957652fb25cad4de0c
akfarrell <akfarrell@mail.usf.edu>
2015-12-04 18:12:10 -0500

    Plane works for distances and using to estimate travel time

--- Plutons_waveform_analysis.m
+++ Plutons_waveform_analysis.m
@@ -6,6 +6,7 @@ close all
 startup_seismiclab
 addpath(genpath('/raid/apps/src/GEOTOOLS/matlab_util'))
 addpath('data_func_matTaup/')
+addpath('latlonutm/Codes/matlab/')
 addpath('readhgt/')
 NEWGISMODIR=fullfile('/raid/apps/matlab/toolbox/GISMO/startup_GISMO.m');
 rmpath(genpath(NEWGISMODIR))
@@ -301,7 +302,7 @@ end
 
 time_vals_ref = [];
 if earthquake_number == 9
-    ref_time = time_values(12);
+    ref_time = time_values(1);
 end
 for values = 1:numel(time_values)
     timelag = (time_values(values)-ref_time)*SECS2DAY;
@@ -413,33 +414,34 @@ visualization(w_clean_sort, Q,eq(earthquake_number).name, fil, eq(earthquake_num
 close all
 
 topo_data = readhgt(-23:-22,-68:-67, 'merge','interp');
-lat = get(w_clean_sort, 'LAT');
-lon = get(w_clean_sort, 'LON');
+lat_sta = get(w_clean_sort, 'LAT');
+lon_sta = get(w_clean_sort, 'LON');
 elev = get(w_clean_sort, 'ELEV');
+[lon, lat] = meshgrid(topo_data.lon, topo_data.lat);
 
 [dist, az_volc_eq] = distance(utu_lat, utu_lon, eq(earthquake_number).lat, eq(earthquake_number).lon);
 [dist2, az_eq_volc] = distance(eq(earthquake_number).lat, eq(earthquake_number).lon, utu_lat, utu_lon);
 
 % ----Calc line of intersection of planar wave with surface at closest station
 if earthquake_number == 9
-    lat_ref = lat(12);
-    lon_ref = lon(12);
+    lat_ref = lat_sta(1);
+    lon_ref = lon_sta(1);
 end
 
 x1 = lat_ref; y1 = lon_ref;
-m = tand(360-az_volc_eq);
+m = tand(az_volc_eq-270);
 C = -m*x1+y1;
 A = m;
 B = -1;
 
 
 for j = 1:numel(sta)
-    [sta_dist(j), az(j)] = distance(lat_ref, lon_ref, lat(j), lon(j));
-    wave_to_sta_dist(j) = (abs(A*(lat(j))+B*(lon(j))+C)/sqrt(A^2 + B^2));
-    y_vals = sind(az_volc_eq)*wave_to_sta_dist(j);
-    x_vals = cosd(az_volc_eq)*wave_to_sta_dist(j);
-    LAT_point_on_line(j) = lat(j)-y_vals;
-    LON_point_on_line(j) = lon(j)-x_vals;
+    [sta_dist(j), az(j)] = distance(lat_ref, lon_ref, lat_sta(j), lon_sta(j));
+    wave_to_sta_dist(j) = (abs(A*(lat_sta(j))+B*(lon_sta(j))+C)/sqrt(A^2 + B^2));
+    y_vals = sind(360-az_volc_eq-90)*wave_to_sta_dist(j);
+    x_vals = cosd(360-az_volc_eq-90)*wave_to_sta_dist(j);
+    LAT_point_on_line(j) = lat_sta(j)-y_vals;
+    LON_point_on_line(j) = lon_sta(j)-x_vals;
 end
 %distOut = distdim(sta_dist, 'degrees', 'km');
 %wave_to_sta_dist_OUT = distdim(wave_to_sta_dist, 'degrees', 'km');
@@ -450,6 +452,74 @@ for i = 1:numel(x)
     y(i) = A*x(i)+C;
 end
 
+%%
+% tolerance = 0.0005;
+% for index = 1:numel(utu_lat)
+%     for index2 = 1:numel(topo_data.lat)
+%         Lat_for_z = find(topo_data.lat(index2)>(utu_lat(index)-tolerance) & topo_data.lat(index2)<(utu_lat(index)+tolerance));
+%         Lon_for_z = find(topo_data.lon(index2)>(utu_lon(index)-tolerance) & topo_data.lon(index2)<(utu_lon(index)+tolerance));
+%         if Lat_for_z~=0
+%             Lat_index_for_z(index) = index2
+%         end
+%         if Lon_for_z~=0
+%             Lon_index_for_z(index) = index2
+%         end
+%     end
+% end
+
+%floor((68-67.186035)/(lat(1,1)-lat(2,1)))-1     to determine matrix coordinates
+%for given lat and lon
+
+for index=1:numel(sta)
+    elev_on_line(index) = interp2(lon, lat, topo_data.z, LON_point_on_line(index), LAT_point_on_line(index)); %Interpolate to find elevation at points on the line
+end
+%%
+%Calculate distance from plane to station with topography
+%elev_on_line, LAT_point_on_line, LON_point_on_line, lat_sta, lon_sta,
+%elev*1000
+
+%----RUN AS A TEST-------%
+%eq(earthquake_number).aoi = 0.0000001;
+%c = (elev - elev(1))*1000
+%Compare c to Distance
+
+close all
+[x_on_line, y_on_line]= ll2utm(LAT_point_on_line, LON_point_on_line); %calculate utm coordinates of points on line, in m
+[x_of_sta, y_of_sta]= ll2utm(lat_sta, lon_sta); %calculate utm coordinates of station locations, in m
+
+diff_on_line = 1000/tand(eq(earthquake_number).aoi);
+if earthquake_number >=6 && earthquake_number<=9
+    third_point_x = x_of_sta(1)+diff_on_line*(sind(az_volc_eq-270));
+    third_point_y = y_of_sta(1)-diff_on_line*(sind(az_volc_eq-270));
+end
+
+[third_point_lat, third_point_lon] = utm2ll(third_point_x, third_point_y, -19); %convert this third point from UTM to lat lon
+
+P1 = [x_of_sta(1),y_of_sta(1), elev(1)*1000]; %first station coordinates (UTM) and elevation
+P2 = [x_on_line(2), y_on_line(2), elev(1)*1000]; %point (UTM) on intersecting line at elevation of first station
+P3 = [third_point_x, third_point_y, elev(1)*1000-10]; %point along the plane under the surface of the earth
+%determine distance between points on line and station
+%line_to_sta_dist= sqrt((x_on_line-x_of_sta).^2+(y_on_line-y_of_sta).^2); %distance from line to station on surface
+
+normal = cross(P1-P2, P1-P3);
+
+x = [P1(1) P2(1) P3(1)];
+y = [P1(2) P2(2) P3(2)];
+z = [P1(3) P2(3) P3(3)];
+
+A = normal(1); B = normal(2); C = normal(3);
+D = -dot(normal, P1);
+nnorm = normal/norm(normal);
+p = D/norm(normal);
+for i = 1:numel(sta)
+    P4 = [x_of_sta(1), y_of_sta(i), elev(i)*1000];
+    Distance(i) = dot(nnorm, P4)+p;
+end
+
+velocity = 4100; %velocity between -6 km and 15 km, in km/s
+travel_time_relativeToFirstStation = Distance/velocity;
+
+%%
 h = figure;
 latlim = [-22.75 -21.75]; %[southern_limit northern_limit] 
 lonlim = [-67.75 -66.5]; %[western_limit eastern_limit]
@@ -467,36 +537,25 @@ minval = min2(2);
 total = maxval+minval;
 plotm(x, y, 'k')
 hold on
-scatterm(lat, lon, '^', 'k')
+scatterm(lat_sta, lon_sta, '^', 'k')
 %textm(LAT_point_on_line, LON_point_on_line, sta)
 scatterm(LAT_point_on_line, LON_point_on_line, 'o', 'k', 'filled')
+scatterm(lat(884,977), lon(884,977), '^', 'filled')
 scatterm(lat_ref, lon_ref, '^', 'k', 'filled')
-textm(lat, lon, sta)
-lat_az = -21.80;
-lon_az = -67.70;
+scatterm(third_point_lat, third_point_lon, 'c', 'filled')
+scatterm(lat_sta(1), lon_sta(1), 'c', 'filled')
+scatterm(LAT_point_on_line(2), LON_point_on_line(2), 'c', 'filled')
+textm(lat_sta, lon_sta, sta)
+lat_az = -21.85;
+lon_az = -67.60;
 hypotenuse = 0.07;
-u = hypotenuse*sind(eq(earthquake_number).az); %vertical
-v = hypotenuse*cosd(eq(earthquake_number).az); %horizontal
+u = hypotenuse*sind(360-eq(earthquake_number).az-90); %vertical
+v = hypotenuse*cosd(360-eq(earthquake_number).az-90); %horizontal
 quiverm(lat_az, lon_az,u, v, 'k')
 directory = sprintf('/home/a/akfarrell/Uturuncu/%s/figures', eq(earthquake_number).name);
 filename = sprintf('%s_wavefront_%1.4f_%1.4f.png',eq(earthquake_number).name,fil(1),fil(2));
 filename_wPath = fullfile(directory,filename);
 hgexport(h, filename_wPath, hgexport('factorystyle'), 'Format', 'png');
-
-%%
-tolerance = 0.0005;
-for index = 1:numel(sta)
-    for index2 = 1:numel(topo_data.lat)
-        Lat_for_z = find(topo_data.lat(index2)>(LAT_point_on_line(index)-tolerance) & topo_data.lat(index2)<(LAT_point_on_line(index)+tolerance));
-        Lon_for_z = find(topo_data.lon(index2)>(LON_point_on_line(index)-tolerance) & topo_data.lon(index2)<(LON_point_on_line(index)+tolerance));
-        if Lat_for_z~=0
-            Lat_index_for_z(index) = index2;
-        end
-        if Lon_for_z~=0
-            Lon_index_for_z(index) = index2;
-        end
-    end
-end
 %%
 g = figure;
 set(g, 'Position', [1000 1000 1000 1000])
